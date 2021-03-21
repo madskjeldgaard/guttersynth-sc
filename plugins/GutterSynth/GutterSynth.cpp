880e136e6a3623190d1396b01e3ba4397acdac4f
scott <scott@artificia.org>
2021-03-21 13:12:36 +0100

    Internal state storage - copied directly from Max implementation

--- plugins/GutterSynth/GutterSynth.cpp
+++ plugins/GutterSynth/GutterSynth.cpp
@@ -7,6 +7,35 @@
 static InterfaceTable* ft;
 
 namespace GutterSynth {
+	
+struct State {
+	public double[][] a0, a1, a2, b1, b2;
+	
+	public double[][] filterFreqsArray, filterFreqsArrayTemp;
+	//public double[] filterFreqsTempArray = {68, 97, 170, 248, 391, 449, 531, 589, 658, 711, 879, 771, 807, 1053, 1200, 1255, 1460, 1478, 1521, 1685, 1666, 1784, 1921, 1954, 68, 97, 170, 248, 391, 449, 531, 589, 658, 711, 879, 771, 807, 1053, 1200, 1255, 1460, 1478, 1521, 1685, 1666, 1784, 1921, 1954};
+	public double[] Q, QTemp;
+	//public double[] QTemp = {30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+	public double[][] V, K, norm, prevX1, prevX2, prevY1, prevY2, y;
+	public double[] gains;
+	public double M_PI, Fs, singleGain;
+	public double audioInput;
+	public boolean enableAudioInput;
+	public int bankCount = 2;
+
+	public double smoothing; // for the lowpass. 1 = no lowpass, 5 = quite lowpassed
+	
+	public double duffX, duffY, dx, dy;
+	public double gamma, omega, c, t, dt;
+	
+	public float[] x;
+	public double finalY;
+
+	public int distMethod = 2;
+	
+	public int filterCount;
+
+	public boolean filtersOn;
+};
 
 GutterSynth::GutterSynth() {
     mCalcFunc = make_calc_function<GutterSynth, &GutterSynth::next>();
