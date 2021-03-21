5dcb27ed19e96ae88110904d600c7cdd784d71df
scott <scott@artificia.org>
2021-03-21 14:27:51 +0100

    Move State declaration to header

--- plugins/GutterSynth/GutterSynth.cpp
+++ plugins/GutterSynth/GutterSynth.cpp
@@ -4,56 +4,10 @@
 #include "SC_PlugIn.hpp"
 #include "GutterSynth.hpp"
 
-#include <array>
-
 static InterfaceTable* ft;
 
 namespace GutterSynth {
 
-constexpr auto DefaultBankCount = 2;
-constexpr auto DefaultFilterCount = 24;
-
-enum class DistortionType {
-	Clipping,
-	CubicClipping,
-	Tanh,
-	Atan,
-	TanhApprox,
-	Sigmoid
-};
-	
-template <size_t BankCount, size_t FilterCount>
-struct State {
-	std::array<std::array<double, FilterCount>, BankCount> a0, a1, a2, b1, b2;
-	
-	std::array<std::array<double, FilterCount>, BankCount> filterFreqsArray, filterFreqsArrayTemp;
-
-	std::array<double, FilterCount> Q, QTemp;
-
-	std::array<std::array<double, FilterCount>, BankCount> V, K, norm, prevX1, prevX2, prevY1, prevY2, y;
-
-	std::array<double, BankCount> gains;
-	double Fs, singleGain;
-	double audioInput;
-	bool enableAudioInput;
-	int bankCount {BankCount}; // @TODO redundant?
-
-	double smoothing; // for the lowpass. 1 = no lowpass, 5 = quite lowpassed
-
-	double duffX, duffY, dx, dy;
-	double gamma, omega, c, t, dt;
-
-	//public float[] x; @TODO unused?
-	double finalY;
-
-	DistortionType distortionType {DistortionType::Tanh};
-
-	int filterCount;
-
-	bool filtersOn;
-};
-
-using GutterState = State<DefaultBankCount, DefaultFilterCount>;
 
 GutterSynth::GutterSynth() {
     mCalcFunc = make_calc_function<GutterSynth, &GutterSynth::next>();
--- plugins/GutterSynth/GutterSynth.hpp
+++ plugins/GutterSynth/GutterSynth.hpp
@@ -4,12 +4,62 @@
 #pragma once
 
 #include "SC_PlugIn.hpp"
+#include <array>
 
 namespace GutterSynth {
+
+constexpr auto DefaultBankCount = 2;
+constexpr auto DefaultFilterCount = 24;
+
+enum class DistortionType {
+	Clipping,
+	CubicClipping,
+	Tanh,
+	Atan,
+	TanhApprox,
+	Sigmoid
+};
+	
+template <size_t BankCount, size_t FilterCount>
+struct State {
+	std::array<std::array<double, FilterCount>, BankCount> a0, a1, a2, b1, b2;
+	
+	std::array<std::array<double, FilterCount>, BankCount> filterFreqsArray, filterFreqsArrayTemp;
+
+	std::array<double, FilterCount> Q, QTemp;
+
+	std::array<std::array<double, FilterCount>, BankCount> V, K, norm, prevX1, prevX2, prevY1, prevY2, y;
+
+	std::array<double, BankCount> gains;
+	double Fs, singleGain;
+	double audioInput;
+	bool enableAudioInput;
+	int bankCount {BankCount}; // @TODO redundant?
+
+	double smoothing; // for the lowpass. 1 = no lowpass, 5 = quite lowpassed
+
+	double duffX, duffY, dx, dy;
+	double gamma, omega, c, t, dt;
+
+	//public float[] x; @TODO unused?
+	double finalY;
+
+	DistortionType distortionType {DistortionType::Tanh};
+
+	int filterCount;
+
+	bool filtersOn;
+};
+
+using GutterState = State<DefaultBankCount, DefaultFilterCount>;
+
+void InitGutterState(GutterState&);
 
 class GutterSynth : public SCUnit {
 public:
     GutterSynth();
+
+    GutterState state;
 
     // Destructor
     // ~GutterSynth();
